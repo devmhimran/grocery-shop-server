2dc41a91d58e6e2c5848e0bb66fcdcd37f23c0fb
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-05-08 17:51:40 +0600

    Added Product Delete

--- grocery-shop-server/index.js
+++ grocery-shop-server/index.js
@@ -39,8 +39,14 @@ async function run(){
       res.send(singleProduct);
     })
 
-    app.delete('/inventory/')
-  
+    app.delete('/inventory/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id : ObjectId(id)};
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    })
+    
+
   }finally{}
 }
 run().catch(console.dir);
