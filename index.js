cc20be36321e08a6f3e7956b47c8445086b16669
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-12-11 12:23:30 +0600

    Update Connect

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
   try{
-    await client.connect();
+    // await client.connect();
     const productCollection = client.db("grocery_shop").collection("products");
 
     app.get('/inventory', async(req, res)=>{
