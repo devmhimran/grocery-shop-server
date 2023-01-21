918a0a56cf8de2d71803995ba3ad89fe58ac2b82
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-01-21 19:57:16 +0600

    Update Index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const res = require('express/lib/response');
+// const res = require('express/lib/response');
 
 app.use(cors());
 app.use(express.json());
