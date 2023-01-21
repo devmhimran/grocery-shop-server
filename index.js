4e7f6c90de2dee53f65884521456917099206b3b
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2023-01-21 20:22:52 +0600

    Update Files

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const res = require('express/lib/response');
 
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dsvrj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
